5eaaaacf2ba6f17ef0d4468408eabee3a0115ad5
Larissa Tater <larissa.tater@gmail.com>
2015-09-10 10:43:40 -0500

    ability to swap turn

--- code/js/javascript.js
+++ code/js/javascript.js
@@ -2,6 +2,7 @@ $(function() {
 
 var $playerOne = {name: null, tile: null, turn: null, moves: [], wins: 0};
 var $playerTwo = {name: null, tile: null, turn: null, moves: [], wins: 0};
+var players = [$playerOne, $playerTwo];
 
 // Introduction
 	// Click start to play Tic Tac Toe!
@@ -20,7 +21,6 @@ var $playerTwo = {name: null, tile: null, turn: null, moves: [], wins: 0};
 				//When you click to submit player 1, store the name in the stat section
 				$playerOne.name = $("#player-one-name").val()
 				$("#player-one-variable").html('Player 1: ' + $playerOne.name);
-				console.log($playerOne.name);
 
 	// Please enter a name for player 2
 				$(".prompts").html("What is the name of Player 2?").append("<input type='text' id='player-two-name'>").append("<input type='button' id='submit-player-two' value='Submit'>");
@@ -29,7 +29,6 @@ var $playerTwo = {name: null, tile: null, turn: null, moves: [], wins: 0};
 			$("#submit-player-two").on("click", function() {
 				$playerTwo.name = $("#player-two-name").val();
 				$("#player-two-variable").html('Player 2: ' + $playerTwo.name);
-				console.log($playerTwo.name);
 
 	// Let's flip a coin to see who goes first
 				$(".prompts").html("Let's flip a coin to see who goes first. " + $playerOne.name + " is heads. " + $playerTwo.name + " is tails.").append("<input type='button' id='coin-flip' value='Flip!'>");
@@ -45,12 +44,11 @@ var $playerTwo = {name: null, tile: null, turn: null, moves: [], wins: 0};
 					//Set whose turn it is to play
 					$playerOne.turn = true;
 					$playerTwo.turn = false;
-					//console.log('P1 true - player 1s turn is' + $playerOne.turn);
-					//console.log('P2 false - player 2s turn is' + $playerTwo.turn);					
+					console.log("player one's turn is " + $playerOne.turn);
+					console.log("player two's turn is " + $playerTwo.turn);
+
 
 					$(".prompts").html("Heads wins! " + coinFlipWinner.name + ", choose X or O.").append("<input type='button' id='tile-x' value='X'>").append("<input type='button' id='tile-o' value='O'>");
-					//console.log(coinFlipWinner);
-					//console.log(coinFlipLoser);
 
 				} else if (coinFlipWinner > 0.5) {
 					coinFlipWinner = $playerTwo;
@@ -59,19 +57,18 @@ var $playerTwo = {name: null, tile: null, turn: null, moves: [], wins: 0};
 					//Set whose turn it is to play
 					$playerOne.turn = false;
 					$playerTwo.turn = true;
-					//console.log('P1 - false player 1s turn is' + $playerOne.turn);
-					//console.log('P2 - true player 2s turn is' + $playerTwo.turn);
+					console.log("player one's turn is " + $playerOne.turn);
+					console.log("player two's turn is " + $playerTwo.turn);
 
 					$(".prompts").html("Tails wins! " + coinFlipWinner.name + ", choose X or O.").append("<input type='button' id='tile-x' value='X'>").append("<input type='button' id='tile-o' value='O'>");
-					//console.log(coinFlipWinner);
-					//console.log(coinFlipLoser);
 
 				} else {
 					$(".prompts").html("Drat. It's a tie. Work this out.");
-					//console.log("Drat. It's a tie. Work this out.");
 				}
 
-			// If the winner chooses X, give the winner X and the loser O
+			// DRY: look at the array of objects and determine turns
+
+			// DRY: Can this be drier? If the winner chooses X, give the winner X and the loser O
 
 			$("#tile-x").on("click", function() {
 				coinFlipWinner.tile = 'X';
@@ -83,7 +80,7 @@ var $playerTwo = {name: null, tile: null, turn: null, moves: [], wins: 0};
 				$(".prompts").html(coinFlipWinner.name + " choose a square.")
 			});
 
-			// If the winner chooses O, give the winner O and the loser X
+			// DRY: Can this be drier? If the winner chooses O, give the winner O and the loser X
 			$("#tile-o").on("click", function() {
 				coinFlipWinner.tile = 'O';
 				coinFlipLoser.tile = 'X';
@@ -102,22 +99,144 @@ var $playerTwo = {name: null, tile: null, turn: null, moves: [], wins: 0};
 
 // Play the game
 
-//var winningCombo = [[a, b, c], [d, e, f], [g, h, i], [a, d, g], [b, e, h], [c, f, i], [a, e, i], [c, e, g]];
+//var $playerOne = {name: null, tile: null, turn: null, moves: [], wins: 0};
+//var $playerTwo = {name: null, tile: null, turn: null, moves: [], wins: 0};
 
-/*var $playerOne = {name: null, tile: 'X', turn: true, moves: [], wins: 0};
-var $playerTwo = {name: null, tile: 'O', turn: false, moves: [], wins: 0};
-
-// Set coinFlipWinner's turn to true;
-// Set coinFlipLoser's turn to false;
-
-$(".tic-square").on("click", function() {
-	if ($playerOne.turn = true){
-		$(this).html($playerOne.tile);
-	} else {
-		$(this).html($playerTwo.tile);
-	}
-});*/
+		// HELP: Look at this array. If .turn is set to true, play .tile. Automatically find the true one without if/else.
+		// If the player's turn is true, when he/she clicks on a square, put his/her tile in it
 
 
+		//Issue: running through all of them because any time you click any old square, all of the functions with that click will run.
+		//Need to get more specific and make it a one action situation.
+
+		function swapTurns(turn) {
+			if ($playerOne.turn === true) {
+				$playerOne.turn = false;
+				$(".prompts").html($playerTwo.name + " choose a square.")
+				console.log("function: player one's new turn is " + $playerOne.turn);
+			} else if ($playerOne.turn === false) {
+				$playerOne.turn = true;
+				$(".prompts").html($playerOne.name + " choose a square.")
+				console.log("function: player one's new turn is " + $playerOne.turn);
+			} else if ($playerTwo.turn === true) {
+				$playerTwo.turn = false;
+				$(".prompts").html($playerOne.name + " choose a square.")
+				console.log("function: player two's new turn is " + $playerTwo.turn);
+			} else if ($playerTwo.turn === false) {
+				$playerTwo.turn = true;
+				$(".prompts").html($playerTwo.name + " choose a square.")
+				console.log("function: player two's new turn is " + $playerTwo.turn);
+			}
+		};
+
+
+		$("#a").on("click", function() {
+			if ($playerOne.turn === true){
+				$("#a").html($playerOne.tile);
+				console.log("#a player one's turn is " + $playerOne.turn);
+			} else if ($playerOne.turn === false) {
+				$("#a").html($playerTwo.tile);
+				console.log("#a player two's turn is " + $playerTwo.turn);
+			}
+
+			swapTurns()
+
+		});
+
+		$("#b").on("click", function() {
+			if ($playerOne.turn === true){
+				$("#b").html($playerOne.tile);
+				console.log("#b player one's turn is " + $playerOne.turn);
+			} else if ($playerOne.turn === false) {
+				$("#b").html($playerTwo.tile);
+				console.log("#b player two's turn is " + $playerTwo.turn);
+			}
+
+			swapTurns()
+		});
+
+		$("#c").on("click", function() {
+			if ($playerOne.turn === true){
+				$("#c").html($playerOne.tile);
+				console.log("#c player one's turn is " + $playerOne.turn);
+			} else if ($playerOne.turn === false) {
+				$("#c").html($playerTwo.tile);
+				console.log("#c player two's turn is " + $playerTwo.turn);
+			}
+
+			swapTurns()
+		});
+
+		$("#d").on("click", function() {
+			if ($playerOne.turn === true){
+				$("#d").html($playerOne.tile);
+				console.log("#c player one's turn is " + $playerOne.turn);
+			} else if ($playerOne.turn === false) {
+				$("#d").html($playerTwo.tile);
+				console.log("#c player two's turn is " + $playerTwo.turn);
+			}
+
+			swapTurns()
+		});
+
+		$("#e").on("click", function() {
+			if ($playerOne.turn === true){
+				$("#e").html($playerOne.tile);
+				console.log("#c player one's turn is " + $playerOne.turn);
+			} else if ($playerOne.turn === false) {
+				$("#e").html($playerTwo.tile);
+				console.log("#c player two's turn is " + $playerTwo.turn);
+			}
+
+			swapTurns()
+		});
+
+		$("#f").on("click", function() {
+			if ($playerOne.turn === true){
+				$("#f").html($playerOne.tile);
+				console.log("#c player one's turn is " + $playerOne.turn);
+			} else if ($playerOne.turn === false) {
+				$("#f").html($playerTwo.tile);
+				console.log("#c player two's turn is " + $playerTwo.turn);
+			}
+
+			swapTurns()
+		});
+
+		$("#g").on("click", function() {
+			if ($playerOne.turn === true){
+				$("#g").html($playerOne.tile);
+				console.log("#c player one's turn is " + $playerOne.turn);
+			} else if ($playerOne.turn === false) {
+				$("#g").html($playerTwo.tile);
+				console.log("#c player two's turn is " + $playerTwo.turn);
+			}
+
+			swapTurns()
+		});
+
+		$("#h").on("click", function() {
+			if ($playerOne.turn === true){
+				$("#h").html($playerOne.tile);
+				console.log("#c player one's turn is " + $playerOne.turn);
+			} else if ($playerOne.turn === false) {
+				$("#h").html($playerTwo.tile);
+				console.log("#c player two's turn is " + $playerTwo.turn);
+			}
+
+			swapTurns()
+		});
+
+		$("#i").on("click", function() {
+			if ($playerOne.turn === true){
+				$("#i").html($playerOne.tile);
+				console.log("#c player one's turn is " + $playerOne.turn);
+			} else if ($playerOne.turn === false) {
+				$("#i").html($playerTwo.tile);
+				console.log("#c player two's turn is " + $playerTwo.turn);
+			}
+
+			swapTurns()
+		});
 
 });
